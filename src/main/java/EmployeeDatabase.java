0a0efd2f4e5b1b24f273720c4ca5d503b76641d8
leivathan <andrewk3@illinois.edu>
2018-03-14 12:44:34 -0500

    i won

--- src/main/java/EmployeeDatabase.java
+++ src/main/java/EmployeeDatabase.java
@@ -15,7 +15,7 @@ public class EmployeeDatabase {
     /**
      * List of employees.
      */
-    public List<Employee> employees;
+    private List<Employee> employees;
 
     /**
      * Constructor which initializes the employees list.
@@ -30,10 +30,10 @@ public class EmployeeDatabase {
     /**
      * Returns the manager for the given employee.
      *
-     * @param employee
-     * @return
+     * @param employee blah blah.
+     * @return ludem ortis et.
      */
-    Employee findManager(final Employee employee) {
+    public Employee findManager(final Employee employee) {
         Employee manager = null;
         for (int i = 0; i < employees.size(); i++) {
             if (employees.get(i).getName() == employee.getManager()) {
@@ -56,6 +56,11 @@ public class EmployeeDatabase {
         /*
          * Implement this function
          */
+        if (findManager(employee) == null) {
+            return 0;
+        } else {
+            return  1 + countManagersAbove(findManager(employee));
+        }
     }
 
     /**
@@ -70,6 +75,13 @@ public class EmployeeDatabase {
         /*
          * Implement this function
          */
+        int count = 0;
+        for (int i = 0; i < employees.size(); i++) {
+            if (findManager(employees.get(i)) == employee) {
+                count += (1 + countEmployeesUnder(employees.get(i)));
+            }
+        }
+        return count;
     }
 
     /**
